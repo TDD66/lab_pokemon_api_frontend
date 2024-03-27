67eebd08d14c5b3d0fce6f40ef04e2cf35a426ff
Tadiwanashe Dzvoti <tadiwadzvoti@hotmail.com>
2024-03-27 12:41:53 +0000

    Fetched 10 pokemons and console logged them.

--- src/containers/PokemonContainer.js
+++ src/containers/PokemonContainer.js
@@ -1,6 +1,28 @@
-import PokemonListContainer from "./PokemonListContainer";
+import { useEffect, useState } from "react";
+import PokemonListContainer from "../components/PokemonList";
 
 const PokemonContainer = () => {
+
+    const [pokemons, setPokemons] = useState([]);
+
+    const fetchPokemons = async () => {
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10");
+        const pokemonData = await response.json();
+
+        const pokemonRequests = pokemonData.results.map(async (pokemon) => {
+            const pokemonResponse = await fetch(pokemon.url);
+            return pokemonResponse.json();
+        });
+
+        Promise.all(pokemonRequests)
+        .then((results) => {
+            console.log(results);
+        });
+    }
+
+    useEffect(() => {
+        fetchPokemons();
+    }, [])
 
     return (  
         <>
